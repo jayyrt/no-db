372af6384eb5b8c67548b47729935c1c8d528459
jayyrt <jayy.rt19@gmail.com>
2021-07-27 16:12:26 -0600

    servers

--- server/index.js
+++ server/index.js
@@ -3,4 +3,4 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.listen(3000, () => console.log('listening on 3000'))
+app.listen(3000, () => console.log('listening on 3000'));
