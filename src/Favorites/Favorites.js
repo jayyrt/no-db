ea9e7a8be93f2eb046fc1b45df9e6c1d57390c51
jayyrt <jayy.rt19@gmail.com>
2021-07-29 11:13:28 -0600

    progress

--- src/Favorites/Favorites.js
+++ src/Favorites/Favorites.js
@@ -1,24 +1,15 @@
-import axios from 'axios';
 import React, { Component } from 'react';
 
 import './Favorites.css'
 
-export default class Favorites extends Component {
+class Favorites extends Component {
     constructor() {
         super();
 
         this.state = {
-            placesVisited: []
+            places: []
         }
 
-        this.getPlaces = this.getPlaces.bind(this);
-    }
-
-    getPlaces() {
-    //   axios.get('')
-    //  .then( res => {
-    //   this.setState({ placesVisited: res.data }) 
-    // })
     }
 
     render(){
@@ -28,9 +19,9 @@ export default class Favorites extends Component {
                 <span>Favorite Places</span>
                 <button className="Get-Places" onClick={this.getPlaces}>Show Favorites</button>
             </section>
-            <section className="Faves-content"></section>
             </div>
-            
         )
     }
 }
+
+export default Favorites;
